197f25fb703bd82ac4c532a8d2d3e376721e87b7
KRISHANxx <150045076+KRISHANxx@users.noreply.github.com>
2024-01-07 09:13:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94764224187', 'KRISHAN', true],
+  ['94764224187', 'KRISHAN', true], 
   [''] 
 ] //Number of owners
 
